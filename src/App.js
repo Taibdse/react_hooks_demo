a7f6d6a1607290a71e80dd3b5180ac97b62a7bfc
Taibdse <buiductai232@gmail.com>
2019-02-14 11:57:42 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import classnames from 'classnames';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function Todo({ todo, index, toggleComplete, deleteTodo }){
+  let { isCompleted, text } = todo;
+  return (
+    <div className="todo list-group-item" style={{ textDecoration: todo.isCompleted ? 'line-through': '' }}>
+      {text}
+      <button className={classnames('badge', { 'badge-success': isCompleted, 'badge-info': !isCompleted })} onClick={() => toggleComplete(index)} 
+      style={{ marginLeft: 20, marginRight: 5 }}>
+      {isCompleted ? 'completed' : 'not completed'}
+      </button>
+      <button className="badge badge-danger" onClick={() => deleteTodo(index)}>delete</button>
+    </div>
+  )
+}
+
+function TodoForm({addTodo}){
+  const [value, setValue] = useState('');
+
+  const handleSUbmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
   }
+
+  return (
+    <form onSubmit={handleSUbmit}>
+      <div className="form-group">
+        <input placeholder="Enter name..." type="text" 
+        className="form-control" value={value} 
+        onChange={e => setValue(e.target.value)}/>
+      </div>
+    </form>
+  )
+}
+
+function App(){
+  const [todos, setTodos] = useState([
+    { text: 'React', isCompleted: true },
+    { text: 'Angular', isCompleted: false },
+    { text: 'Vue', isCompleted: false },
+    { text: 'Nodejs', isCompleted: true },
+    { text: 'Expresjs', isCompleted: false },
+    { text: 'Mongodb', isCompleted: false },
+    { text: 'Webpack', isCompleted: true },
+    { text: 'Babel', isCompleted: false },
+    { text: 'Parcel', isCompleted: false },
+    { text: 'Grunt', isCompleted: true },
+    { text: 'NPM', isCompleted: false },
+    { text: 'Jquery', isCompleted: true },
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text, isCompleted: false }];
+    setTodos(newTodos);
+  }
+
+  const toggleComplete = index => {
+    // console.log(index);
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  }
+  
+  const deleteTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="app container">
+     <div className="row mt-4">
+      <div className="col-md-8 mx-auto">
+        <TodoForm addTodo={addTodo} />
+        <div className="todo-list list-group">
+          {todos.map((todo, index) => (
+            <Todo 
+            key={index} 
+            index={index} 
+            todo={todo} 
+            toggleComplete={toggleComplete} 
+            deleteTodo={deleteTodo}/>
+          ))}
+        </div>
+      </div>
+     </div>
+    </div>
+  )
 }
 
 export default App;
